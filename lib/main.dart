230293e2cd285be9cb56234ff71921951eee1db7
Devin Herath <devdhera@gmail.com>
2018-08-27 18:57:47 +0530

    Added image card and report button

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Kiyamuda'),
-        ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/food.jpg'),
-              Text('Fixing the World')
-            ],
+          appBar: AppBar(
+            title: Text('Kiyamuda'),
           ),
-        ),
-      ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Report It!'),
+                ),
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assets/food.jpg'),
+                    Text('Fixing the World')
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
