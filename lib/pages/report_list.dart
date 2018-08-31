3772704878a10ee2e02c01f46464a9d1e55cc865
Devin Herath <devdhera@gmail.com>
2018-08-31 17:09:18 +0530

    Added tab navigation for admin page

---
+++ lib/pages/report_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReportListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('All Reports'),
+    );
+  }
+}
--- lib/pages/reports_admin.dart
+++ lib/pages/reports_admin.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 import './reports.dart';
+import './report_create.dart';
+import './report_list.dart';
 
 class ReportsAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
         drawer: Drawer(
           child: Column(
             children: <Widget>[
@@ -27,9 +31,23 @@ class ReportsAdminPage extends StatelessWidget {
         ),
         appBar: AppBar(
           title: Text('Manage Reports'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.create),
+                text: 'Create Report',
+              ),
+              Tab(
+                icon: Icon(Icons.list),
+                text: 'My Reports',
+              ),
+            ],
+          ),
         ),
-        body: Center(
-          child: Text('Manage Your Reports'),
-        ));
+        body: TabBarView(
+          children: <Widget>[ReportCreatePage(), ReportListPage()],
+        ),
+      ),
+    );
   }
 }
