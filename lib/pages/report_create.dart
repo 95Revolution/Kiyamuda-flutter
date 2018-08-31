3e28f23fe747f4db64eab32383701571fd9a0ffc
Devin Herath <devdhera@gmail.com>
2018-08-31 21:01:57 +0530

    Added basic input fields

--- lib/pages/report_create.dart
+++ lib/pages/report_create.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
 
-class ReportCreatePage extends StatelessWidget {
+class ReportCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ReportCreatePage();
+  }
+}
+
+class _ReportCreatePage extends State<ReportCreatePage> {
+  String titleValue;
+  String descriptionValue;
+  double rateValue;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Save'),
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) {
-                return Center(
-                  child: Text('Modal Test!!!'),
-                );
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'Report Title'),
+            onChanged: (String value) {
+              setState(() {
+                titleValue = value;
               });
-        },
+            },
+          ),
+          TextField(
+            maxLines: 4,
+            decoration: InputDecoration(labelText: 'Report Description'),
+            onChanged: (String value) {
+              setState(() {
+                descriptionValue = value;
+              });
+            },
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(labelText: 'Rate it'),
+            onChanged: (String value) {
+              setState(() {
+                rateValue = double.parse(value);
+              });
+            },
+          ),
+        ],
       ),
     );
   }
