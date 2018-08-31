6eefcbe5a8da647fc199f3d93e732e29ed65c74c
Devin Herath <devdhera@gmail.com>
2018-08-31 16:28:03 +0530

    Added Sidedrawer

--- lib/pages/reports.dart
+++ lib/pages/reports.dart
@@ -6,6 +6,20 @@ class ReportsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text('Choose'),
+              ),
+              ListTile(
+                title: Text('Manage Reports'),
+                onTap: () {},
+              )
+            ],
+          ),
+        ),
         appBar: AppBar(
           title: Text('Kiyamuda'),
         ),
