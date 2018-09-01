1f0009e8b1a83ecc736610220443486eb4653f29
Devin Herath <devdhera@gmail.com>
2018-09-01 10:34:18 +0530

    Added Rate Tag

--- lib/reports.dart
+++ lib/reports.dart
@@ -11,15 +11,33 @@ class Reports extends StatelessWidget {
         children: <Widget>[
           Image.asset(reports[index]['image']),
           Container(
-            padding: EdgeInsets.only(top: 10.0),
-            child: Text(
-              reports[index]['title'],
-              style: TextStyle(
-                  fontSize: 26.0,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Oswald'),
-            ),
-          ),
+              padding: EdgeInsets.only(top: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    reports[index]['title'],
+                    style: TextStyle(
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Oswald'),
+                  ),
+                  SizedBox(
+                    width: 8.0,
+                  ),
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).accentColor,
+                        borderRadius: BorderRadius.circular(5.0)),
+                    child: Text(
+                      reports[index]['rate'].toString(),
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              )),
           ButtonBar(
             alignment: MainAxisAlignment.center,
             children: <Widget>[
