dfb8ea9f94322a641274c857abae71e49d72de16
Devin Herath <devdhera@gmail.com>
2018-09-01 18:30:23 +0530

    Changed UI on report list

--- lib/pages/report_list.dart
+++ lib/pages/report_list.dart
@@ -12,27 +12,33 @@ class ReportListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          leading: CircleAvatar(
-            backgroundImage: AssetImage(reports[index]['image']),
-          ),
-          title: Text(reports[index]['title']),
-          trailing: IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (BuildContext context) {
-                    return ReportEditPage(
-                      report: reports[index],
-                      updateReport: updateReport,
-                      reportIndex: index,
-                    );
-                  },
-                ),
-              );
-            },
-          ),
+        return Column(
+          children: <Widget>[
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(reports[index]['image']),
+              ),
+              title: Text(reports[index]['title']),
+              subtitle: Text('Rating : ${reports[index]['rate']}'),
+              trailing: IconButton(
+                icon: Icon(Icons.edit),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return ReportEditPage(
+                          report: reports[index],
+                          updateReport: updateReport,
+                          reportIndex: index,
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+            Divider(),
+          ],
         );
       },
       itemCount: reports.length,
